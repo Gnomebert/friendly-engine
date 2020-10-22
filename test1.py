a2cf3d3380437afad702fce8e769846431dcfc31
Gnomebert <56593591+Gnomebert@users.noreply.github.com>
2020-10-22 17:11:35 +0100

    commit2.3

--- test1.py
+++ test1.py
@@ -1,3 +1,3 @@
 print('\n master ff, commit 2, make another change after a third commit\n')
 #tag second commit
-#Is this onto a detached head
+#Is this onto a detached head, which therefor cannot be published. ie a non-latest commit to master, was subsequently ammended
